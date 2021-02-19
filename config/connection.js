c65026400e370fa83f5144ad36a16ad761cebbaa
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-18 21:15:05 -0500

    Added Skeleton.

---
+++ config/connection.js
@@ -0,0 +1,29 @@
+// Set up MySQL connection.
+const mysql = require('mysql');
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+
+require("dotenv").config();
+// console.log(process.env.SECRETPASSWORD)
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: process.env.SECRETPASSWORD,
+  database: '', //Enter database here
+});
+};
+// Make connection.
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
