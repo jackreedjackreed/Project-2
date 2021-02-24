6dff30bab1b19d09d63cbfab9dac5660c7dabb76
VinceH28 <VincentHart28@gmail.com>
2021-02-24 01:32:09 -0500

    Update user-cards.js
    added .onload function to add html elements to page to display cards.

--- models/user-cards.js
+++ models/user-cards.js
@@ -24,4 +24,22 @@ module.exports = (sequelize, DataTypes) => {
 
         return cards;
     };
+
+    window.onload = function() {
+
+        for(var i=0; i < myDeck.length; i++){
+            div = document.createElement('div');
+            div.className = 'card';
+    
+            if(myDeck[i].suit == 'Diamonds'){
+                var ascii_char = '♦';
+            } else {
+                var ascii_char = '&' + myDeck[i].suit.toLowerCase() + ';';
+            }
+    
+            div.innerHTML = '' + myDeck[i].name + '' + ascii_char + '';
+            document.body.appendChild(div);
+        }
+    
+    };
 }
