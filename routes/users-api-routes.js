d3d89b641e7f8c0c955ed090729f995078ddaf96
VinceH28 <VincentHart28@gmail.com>
2021-02-24 01:20:27 -0500

    Create users-api-routes.js
    created "function creatingUsers" & .get, .post, and .delete routes

---
+++ routes/users-api-routes.js
@@ -0,0 +1,47 @@
+const db = require('../models');
+
+module.exports = (app) => {
+  //Function for creating users:
+  function creatingUsers(firstName, lastName, favoriteBook) {
+    return users.create({
+      firstName: firstName,
+      lastName: lastName,
+      favoriteBook: favoriteBook,
+    });
+  }
+
+//Inside of users table:
+users.sync({ force: true }).then(function () {
+  creatingUsers("Jim", "Carrey", "The Mask");
+  creatingUsers("Meryl", "Streep", "The Devil Wears Prada");
+  creatingUsers("Sponge", "Bob", "How to Make The Perfect Krabby Patty");
+  creatingUsers("Jack", "Nicholson", "How to Get Away with Murder");
+});
+
+app.get('/api/users', (req, res) => {
+    db.users.findAll({
+      include: [db.Post],
+    }).then((dbusers) => res.json(dbusers));
+  });
+
+  app.get('/api/users/:id', (req, res) => {
+    db.users.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [db.Post],
+    }).then((dbusers) => res.json(dbusers));
+  });
+
+  app.post('/api/users', (req, res) => {
+    db.users.create(req.body).then((dbusers) => res.json(dbusers));
+  });
+
+  app.delete('/api/users/:id', (req, res) => {
+    db.users.destroy({
+      where: {
+        id: req.params.id,
+      },
+    }).then((dbusers) => res.json(dbusers));
+  });
+}
