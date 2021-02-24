f1863dfa1a35ced9c30f957de06e89c4ee3498cd
VinceH28 <VincentHart28@gmail.com>
2021-02-24 01:16:54 -0500

    Update book.js
    added "module.exports" & modified "type"

--- models/book.js
+++ models/book.js
@@ -1,15 +1,15 @@
-//Table for books
+module.exports = (sequelize, DataTypes) => {
 var books = sequelize.define(
     "books",
     {
       Title: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
       },
       Author: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
       },
       Genre: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
       },
     },
     {
@@ -17,63 +17,33 @@ var books = sequelize.define(
     }
   );
   
-  //Function for creating books:
-  function creatingBooks(title, author, genre) {
-    return Books.create({
-      Title: title,
-      Author: author,
-      Genre: genre,
-    });
-  }
-  
-  //Inside of books table:
-  books.sync({ force: true }).then(function () {
-    creatingBooks("Jimmy's Blues and Other Poems", "James Baldwin", "Poetry");
-    creatingBooks("White Noise", "Don Delillo", "Fiction");
-    creatingBooks("Milk and Honey", "Rupi Kaur", "Poetry");
-    creatingBooks("Harry Potter and the Cursed Child", "J.K. Rowling", "Fantasy");
-  });
-  
   //Table for clubs
   var clubs = sequelize.define(
     "clubs",
     {
       clubName: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
       },
       clubPassword: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
       },
     },
     { freezeBooks: true }
   );
   
-  //Function for creating clubs
-  function creatingClubs(clubName, clubPassword) {
-    return clubs.create({
-      clubName: clubName,
-      clubPassword: clubPassword,
-    });
-  }
-  
-  //Inside of clubs table:
-  clubs.sync({ force: true }).then(function () {
-    creatingClubs("Brick by Brick", "password1");
-    creatingClubs("Social Norms", "password2");
-  });
-  
+ 
   //Table for a user
   var users = sequelize.define(
     "users",
     {
       firstName: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
       },
       lastName: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
       },
       favoriteBook: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
       },
     },
     {
@@ -97,3 +67,5 @@ var books = sequelize.define(
     creatingUsers("Sponge", "Bob", "How to Make The Perfect Krabby Patty");
     creatingUsers("Jack", "Nicholson", "How to Get Away with Murder");
   });
+
+}
