b9408c66efb5bb8fe6cc511a1d2e1946c240d4e7
VinceH28 <VincentHart28@gmail.com>
2021-02-24 01:19:38 -0500

    Create books-api-routes.js
    created "function creatingBooks" & .get, .post, and .delete routes

---
+++ routes/books-api-routes.js
@@ -0,0 +1,47 @@
+const db = require('../models');
+
+module.exports = (app) => {
+ //Function for creating books:
+ function creatingBooks(title, author, genre) {
+    return Books.create({
+      Title: title,
+      Author: author,
+      Genre: genre,
+    });
+  }
+  
+  //Inside of books table:
+  books.sync({ force: true }).then(function () {
+    creatingBooks("Jimmy's Blues and Other Poems", "James Baldwin", "Poetry");
+    creatingBooks("White Noise", "Don Delillo", "Fiction");
+    creatingBooks("Milk and Honey", "Rupi Kaur", "Poetry");
+    creatingBooks("Harry Potter and the Cursed Child", "J.K. Rowling", "Fantasy");
+  });
+
+  app.get('/api/books', (req, res) => {
+    db.books.findAll({
+      include: [db.Post],
+    }).then((dbbooks) => res.json(dbbooks));
+  });
+
+  app.get('/api/books/:id', (req, res) => {
+    db.books.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [db.Post],
+    }).then((dbbooks) => res.json(dbbooks));
+  });
+
+  app.post('/api/books', (req, res) => {
+    db.books.create(req.body).then((dbbooks) => res.json(dbbooks));
+  });
+
+  app.delete('/api/books/:id', (req, res) => {
+    db.books.destroy({
+      where: {
+        id: req.params.id,
+      },
+    }).then((dbbooks) => res.json(dbbooks));
+  });
+}
