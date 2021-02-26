95506dadddeda6f550befd88c3bbe64d710a5f33
VinceH28 <VincentHart28@gmail.com>
2021-02-26 02:25:13 -0500

    Update book.js
    updated file

--- models/book.js
+++ models/book.js
@@ -1,71 +1,24 @@
+const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
+
 module.exports = (sequelize, DataTypes) => {
-var books = sequelize.define(
+  //Table for books
+  var Books = sequelize.define(
     "books",
     {
-      Title: {
+      title: {
         type: DataTypes.STRING,
       },
-      Author: {
+      author: {
         type: DataTypes.STRING,
       },
-      Genre: {
+      genre: {
         type: DataTypes.STRING,
       },
     },
     {
       freezeBooks: true,
+      timestamps: false
     }
   );
-  
-  //Table for clubs
-  var clubs = sequelize.define(
-    "clubs",
-    {
-      clubName: {
-        type: DataTypes.STRING,
-      },
-      clubPassword: {
-        type: DataTypes.STRING,
-      },
-    },
-    { freezeBooks: true }
-  );
-  
  
-  //Table for a user
-  var users = sequelize.define(
-    "users",
-    {
-      firstName: {
-        type: DataTypes.STRING,
-      },
-      lastName: {
-        type: DataTypes.STRING,
-      },
-      favoriteBook: {
-        type: DataTypes.STRING,
-      },
-    },
-    {
-      freezeBooks: true,
-    }
-  );
-  
-  //Function for creating users:
-  function creatingUsers(firstName, lastName, favoriteBook) {
-      return Books.create({
-        firstName: firstName,
-        lastName: lastName,
-        favoriteBook: favoriteBook,
-      });
-    }
-  
-  //Inside of users table:
-  users.sync({ force: true }).then(function () {
-    creatingUsers("Jim", "Carrey", "The Mask");
-    creatingUsers("Meryl", "Streep", "The Devil Wears Prada");
-    creatingUsers("Sponge", "Bob", "How to Make The Perfect Krabby Patty");
-    creatingUsers("Jack", "Nicholson", "How to Get Away with Murder");
-  });
-
-}
+  return Books;
