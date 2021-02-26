5609492fb85eb65ca4d76b2b46e3cfee95983ee5
VinceH28 <VincentHart28@gmail.com>
2021-02-26 02:27:43 -0500

    Update post.js
    file updated

--- models/post.js
+++ models/post.js
@@ -1,29 +1,30 @@
+const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
+
+
 module.exports = (sequelize, DataTypes) => {
-    const Post = sequelize.define('Post', {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1],
-        },
-      },
-      body: {
-        type: DataTypes.TEXT,
-        allowNull: false,
+  const Post = sequelize.define('Post', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
         len: [1],
       },
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1],
+    },
+  });
+
+  Post.associate = (models) => {
+    Post.belongsTo(models.users, {
+      foreignKey: {
+        allowNull: false,
+      },
     });
-  
-    Post.associate = (models) => {
-      // We're saying that a Post should belong to an Author
-      // A Post can't be created without an Author due to the foreign key constraint
-      Post.belongsTo(models.Author, {
-        foreignKey: {
-          allowNull: false,
-        },
-      });
-    };
-  
-    return Post;
+  // console.log({models})
   };
-  
+
+  return Post;
+};
