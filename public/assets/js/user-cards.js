45b977039e81c18cc52b90ca67df9dce7d104c4c
VinceH28 <VincentHart28@gmail.com>
2021-02-26 01:03:53 -0500

    Create user-cards.js
    modifying functionality based on amended app capabilities

---
+++ public/assets/js/user-cards.js
@@ -0,0 +1,46 @@
+function card(club, user_name, favorite_book, suit){
+	    this.club = club;
+	    this.name = user_name;
+        this.favorite_book = favorite_book;
+	    this.suit = suit;
+    };
+
+    // function deck(){
+    //     this.club =  clubName;
+	//     this.user_name = firstName, lastName;
+    //     this.favorite_book = favoriteBook
+	//     this.suits = ['Hearts','Diamonds','Spades','Clubs'];
+
+	//     var cards = [];
+        
+    //     //
+    //     for (var s = 0; s < this.suits.length; s++ ) {
+    //         for( var n = 0; n < this.club.length; n++ ) {
+    //             cards.push( new card( n+1, this.club[n], this.suits[s] ) );
+    //         }
+    //     }
+
+    //     return cards;npx tailwindcss init
+    // };
+
+    // Test to see if deck prints out in consule
+    var myDeck = new deck();
+    console.log(myDeck);
+
+    window.onload = function() {
+
+        for(var i=0; i < myDeck.length; i++){
+            div = document.createElement('div');
+            div.className = 'card';
+    
+            if(myDeck[i].suit == 'Diamonds'){
+                var ascii_char = '♦';
+            } else {
+                var ascii_char = '&' + myDeck[i].suit.toLowerCase() + ';';
+            }
+    
+            div.innerHTML = '' + myDeck[i].name + '' + ascii_char + '';
+            document.body.appendChild(div);
+        }
+    
+    };
