ce0ce456c66919487472e22676d7d2f5e7819029
VinceH28 <VincentHart28@gmail.com>
2021-02-26 02:40:54 -0500

    Update clubs -api-routes.js
    updated file

--- routes/clubs -api-routes.js
+++ routes/clubs -api-routes.js
@@ -1,20 +1,6 @@
 const db = require('../models');
 
 module.exports = (app) => {
- //Function for creating clubs
- function creatingClubs(clubName, clubPassword) {
-    return clubs.create({
-      clubName: clubName,
-      clubPassword: clubPassword,
-    });
-  }
-  
-  //Inside of clubs table:
-  clubs.sync({ force: true }).then(function () {
-    creatingClubs("Brick by Brick", "password1");
-    creatingClubs("Social Norms", "password2");
-  });
-
   app.get('/api/clubs', (req, res) => {
     db.clubs.findAll({
       include: [db.Post],
@@ -42,4 +28,18 @@ module.exports = (app) => {
     }).then((dbclubs) => res.json(dbclubs));
   });
 
-}
+  
+  function creatingClubs(clubName, clubPassword) {
+  return clubs.create({
+    clubName: clubName,
+    clubPassword: clubPassword,
+  });
+  }
+
+  //Inside of clubs table:
+  clubs.sync({ force: true }).then(function () {
+  creatingClubs("Brick by Brick", "password1");
+  creatingClubs("Social Norms", "password2");
+  });
+
+};
