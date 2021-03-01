35ddb169105e9237076a6010eac5cb2432e61132
VinceH28 <VincentHart28@gmail.com>
2021-03-01 02:39:16 -0500

    Update user-cards.js
    updated file

--- public/js/user-cards.js
+++ public/js/user-cards.js
@@ -5,49 +5,114 @@ document.addEventListener('DOMContentLoaded', (event) => {
         console.log('DOM loaded! 🚀');
     }
 
-    const users;
-    // grab users from db
-    const findUsers = (users) => {
-        let userProfileID = users || '';
-        if (userProfileID) {
-            userProfileID = categoryString.replace(' ' , ' ');
-            userProfileID = `user/${userProfileID}`;
-        }
+    // display a user
+    const renderUser = (user) => {
+      userId = user || '';
+      if (userId) {
+          userId = `/?user${userId}`;
+      }
 
-        fetch(`/api/posts/${userProfileID}`,{
-          method: 'GET',
-          headers: {
-              'Content-Type': 'application/json',
-          },  
-        })
-          .then((response) => response.json())
-          .then((data) => {
-              console.log('Success in getting user:', data);
-              users = data;
-          }) 
+      fetch(`/api/users${userId}`, (req, res) => {
+      db.User.findOne({
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        where: {
+          id: req.params.id
+        },
+      });
+     })
+     .then((dbUser) => res.json(dbUser));
+    
+
+      // Front end call to DELETE a User
+      const deleteUser = (id) => {
+      fetch(`/api/users/${userId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        }).then(renderUser());
+        };
     };
 
-    // initial list of users
-    findUsers();
+    const createNewCard = (card) => {
+        console.log('createNewCard -> card', card);
+    
+        const formattedDate = new Date(card.createdAt).toLocaleDateString();
+    
+        const newUserCard = document.createElement('div');
+        newUserCard.classList.add('card');
+    
+        const newUserCardHeading = document.createElement('div');
+        newUserCardHeading.classList.add('card-header');
+    
+        // Delete button
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'x';
+        deleteBtn.classList.add('delete', 'btn', 'btn-danger');
+        deleteBtn.addEventListener('click', handleUserDelete);
 
-    const insertUser = (userData) => {
-        fetch('/api/users', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(userData),
-        })
-          .then(getUsers)
-          .catch((err) => console.error(err));
-      };
-    // display a user
-    const renderUser = (id) => {
-        fetch(`/api/users/${id}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }).then(getPosts());
+        // Edit button
+        const editButton = document.createElement('button');
+        editButton.textContent = 'EDIT';
+        editButton.classList.add('edit', 'btn', 'btn-info');
+        editButton.addEventListener('click', handleUserEdit);
+
+        const newUserName = document.createElement('h5');
+        const newUserDate = document.createElement('small');
+        const newUserFirst_LastName = document.createdElment('h2');
+        const newUserFavoriteBook = document.createElement('h2');
+        const newUserLocation = document.createElement('h2');
+        const newUserClub = document.createElement('h3');
+        
+        
+        newUserName.textContent = `${userId}`;
+        newUserName.style.float = 'right';
+        newUserName.style.color = 'blue';
+        newUserName.style.marginTop = '-10px';
+
+        const newUserCardBody = document.createElement('div');
+        newUserCardBody.classList.add('card-body');
+
+        const newCardBody = document.createElement('li')
+        newCardBody = post.body;
+        newUserDate.textContent = `(${formattedDate})`;
+        newUserFirst_LastName.textContent = `${user.first.name}, ${user.last.name}`;
+        newUserFavoriteBook.textContent = `${favorite.book}`;
+        newUserLocation.textContent = `${user.location}`;
+        newUserClub.textContent = `${user.club}`;
+        newUserName.append(newUserDate);
+        newUserCardHeading.append(deleteBtn);
+        newUserCardHeading.append(editButton);
+        newUserCardHeading.append(newUserName);
+        newUserCardBody.append(newCardBody);
+        newUserCard.append(newUserName);
+        newUserCard.append(newUserCardBody);
+        newUserCard.setAttribute('data-post', JSON.stringify(post));
+
+        console.log('createNewUser -> newUserCard', newUserCard);
+
+        return newUserCard;
+    };
+    
+    // Handle when we click the delete post button
+    const handleUserDelete = (e) => {
+      const currentUser = JSON.parse(
+        e.target.parentElement.parentElement.dataset.post
+      );
+
+        deletePost(currentUser.id);
+    };
+
+    // Handle when we click the edit post button
+    const handleUserEdit = (e) => {
+      const currentUser = JSON.parse(
+        e.target.parentElement.parentElement.dataset.post
+      );
+
+      window.location.href = `/users?user_id=${currentUser.id}`;
     };
 })
+
