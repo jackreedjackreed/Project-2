e2142a4b8a3a7003911d8179585c38fdc813e7e4
VinceH28 <VincentHart28@gmail.com>
2021-03-01 00:25:27 -0500

    Update user.js
    modified file

--- public/js/user.js
+++ public/js/user.js
@@ -2,6 +2,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM loaded! 🚀');
 
+  const userNameInput = document.getElementById('first-name, last-name');
+  const favoriteBookInput = document.write(text)('favorite-book');
+  const locationInput = document.document.write(text)('location');
+
   // Create a user
   const insertUser = (userData) => {
     fetch('/api/users', {
@@ -15,6 +19,38 @@ document.addEventListener('DOMContentLoaded', () => {
       .catch((err) => console.error(err));
   };
 
+  // Handle when the author form is submitted
+  const handleUserFormSubmit = (e) => {
+    e.preventDefault();
+  
+    if (!userNameInput.value.trim()) {
+      alert('Please provide an user name');
+      return;
+      }
+  
+    insertUser({
+      userName: userNameInput.value.trim(),
+      favoriteBook: favoriteBookInput.value.trim(),
+      location: locationInput.value.trim()
+      });
+  };
+
+  document
+  .getElementById('user-form')
+  .addEventListener('submit', handleUserFormSubmit);
+
+  // Event handler for the delete author button
+  const handleDeleteButtonPress = (e) => {
+    const { id } = e.target.parentElement.parentElement;
+    fetch(`/api/users/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(getUsers);
+  };
+
+
   // Create list row for users
   const createUserRow = (userData) => {
     const tr = document.createElement('tr');
@@ -26,6 +62,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const td = document.createElement('td');
     td.textContent = userData.name;
     tr.appendChild(td);
+
+    // Element to show how many posts
+    const lengthTd = document.createElement('td');
+    if (userData.Posts) {
+      lengthTd.textContent = userData.Posts.length;
+    } else {
+      lengthTd.textContent = '0';
+    }
+    tr.appendChild(lengthTd);
+
+    // "Go to posts" link
+    const postsLink = document.createElement('td');
+    postsLink.innerHTML = `<td><a href='/posts?user_id=${userData.id}'> Go to Posts </a></td>`;
+    tr.appendChild(postsLink);
+   
+    // "Create a post" link
+    const createLink = document.createElement('td');
+    createLink.innerHTML = `<td><a href='/new-posts?user_id=${userrData.id}'> Create a Post </a></td>`;
+    tr.appendChild(createLink);
+   
+    // "Delete author" link
+    const deleteLink = document.createElement('td');
+    deleteLink.innerHTML = `<td><a style='/user-manager?user_id=${userrData.id}'cursor:pointer;color:red' class='delete-user'> Delete User </a></td>`;
+    deleteLink.addEventListener('click', handleDeleteButtonPress);
+    tr.appendChild(deleteLink);
+    
 
     return tr;
   };
