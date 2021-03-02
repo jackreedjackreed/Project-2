5fc8143ba37cd5c5966221f9c66d033504dc79a6
J-Boogie <jackr353@gmail.com>
2021-03-01 18:05:30 -0600

    adding reference code, which veronika built, some new style pages, and the single post html and handlebars

--- controllers/user-api-controller.js
+++ controllers/user-api-controller.js
@@ -1,6 +1,24 @@
 const db = require('../models');
-
 module.exports = (app) => {
+  app.get('/reference', async (req, res) => {
+    try{
+      // Get A Noun
+    const randomNoun = await  db.noun.findAll({
+          order: db.Sequelize.literal('rand()'),
+          limit: 1
+     })
+     // Get an Adjective
+     const randomAdjective = await db.adjective.findAll({
+         order: db.Sequelize.literal('rand()'),
+         limit: 1
+     })
+     const result = {noun: randomNoun[0].noun, adjective: randomAdjective[0].adjective};
+     // Send to the view
+     res.render('reference', {result});
+    }catch(err){  
+      console.log(err);
+    }
+  });
   app.get('/api/users', (req, res) => {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
@@ -9,7 +27,6 @@ module.exports = (app) => {
       include: [db.Post],
     }).then((dbUser) => res.json(dbUser));
   });
-
   app.get('/api/users/:id', (req, res) => {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
@@ -21,11 +38,9 @@ module.exports = (app) => {
       include: [db.Post],
     }).then((dbUser) => res.json(dbUser));
   });
-
   app.post('/api/users', (req, res) => {
     db.User.create(req.body).then((dbUser) => res.json(dbUser));
   });
-
   app.delete('/api/users/:id', (req, res) => {
     db.User.destroy({
       where: {
